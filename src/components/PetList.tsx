20edcab298cdaf531e47e33113a66a3968b484b5
TinglanWei <127041098+TinglanWei@users.noreply.github.com>
2024-05-22 19:00:48 -0400

    add card margin

--- src/components/PetList.tsx
+++ src/components/PetList.tsx
@@ -3,19 +3,24 @@ import { Pet } from "../models/pet";
 import { getPets } from "../services/petService";
 import { Col, Row } from "react-bootstrap";
 import { PetCard } from "./PetCard";
+import "./css/PetList.css";
 
-export function PetList(){
-    const [pets, setPets] = useState<Pet[]>([]);
+export function PetList() {
+  const [pets, setPets] = useState<Pet[]>([]);
 
-    useEffect(() => {
-        getPets().then((pets) => setPets(pets));
-    }, []);
+  useEffect(() => {
+    getPets().then((pets) => setPets(pets));
+  }, []);
 
-    return(
-        <div className="PetList">
-            <Row>
-                {pets.map((pet) => (<Col lg={4}><PetCard pet={pet} /></Col>))}
-            </Row>
-        </div>
-    )
+  return (
+    <div className="PetList">
+      <Row className="g-4 justify-content-center">  {/* g-4 provides gap, justify-content-center centers the items */}
+        {pets.map((pet) => (
+          <Col lg={4} md={6} sm={12} key={pet.id}> {/* Responsive columns */}
+            <PetCard pet={pet} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 }
