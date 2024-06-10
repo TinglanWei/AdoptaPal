bd5d3c221942154ee4eca4ddbd4e6ee8cc75fb84
TinglanWei <127041098+TinglanWei@users.noreply.github.com>
2024-06-10 05:45:58 -0400

    use Route in link navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
+import React from 'react';
 import { Navbar, Container, Nav, NavDropdown, Form, Button, FormControl } from 'react-bootstrap';
 import './css/Header.css';
+import { Link } from "react-router-dom";
 
 export function Header() {
     return (
         <Navbar fixed="top" bg="dark" variant="dark" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="/AdoptaPal">
+                <Navbar.Brand as={Link} to="/AdoptaPal">
                     <img src="https://i.imgur.com/EyRaLKR.jpeg" alt="AdoptaPal Logo" width="50" height="50" />
                     AdoptaPal
                 </Navbar.Brand>
@@ -13,13 +15,13 @@ export function Header() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <NavDropdown title="Discover Pets" id="discover-pets-dropdown">
-                            <NavDropdown.Item href="/AdoptaPal/dogs">Dogs</NavDropdown.Item>
-                            <NavDropdown.Item href="/AdoptaPal/cats">Cats</NavDropdown.Item>
-                            <NavDropdown.Item href="/AdoptaPal/others">Other Animals</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/AdoptaPal/dogs">Dogs</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/AdoptaPal/cats">Cats</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/AdoptaPal/others">Other Animals</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="/AdoptaPal/find-owner">Find Owner</Nav.Link>
-                        <Nav.Link href="/AdoptaPal/adoption-guidelines">Adoption Guidelines</Nav.Link>
-                        <Nav.Link href="/AdoptaPal/about-us">About Us</Nav.Link>
+                        <Nav.Link as={Link} to="/AdoptaPal/find-owner">Find Owner</Nav.Link>
+                        <Nav.Link as={Link} to="/AdoptaPal/adoption-guidelines">Adoption Guidelines</Nav.Link>
+                        <Nav.Link as={Link} to="/AdoptaPal/about-us">About Us</Nav.Link>
                     </Nav>
                     <Form className="d-flex">
                         <FormControl type="text" placeholder="Search pets" className="ml-3" />
